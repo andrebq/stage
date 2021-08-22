98cf56f88a67e477874b98ea59a91214f49a8493
André <andrebq@gmail.com>
2021-08-22 21:24:29 +0200

    chore: add some sleep delay between ping calls

--- example/scripted/jtb/ping.js
+++ example/scripted/jtb/ping.js
@@ -1,5 +1,6 @@
-let io = require("@stdio")
-let actor = require("@actor")
+let io = require("@stdio");
+let actor = require("@actor");
+let sleep = require("@sleep");
 
 io.println("waiting for the first message");
 io.println("sending my salutes to pong actor");
@@ -9,4 +10,5 @@ while (true) {
 	actor.write("pong", JSON.stringify({ hello: "world" }));
 	io.println("waiting for the next message...");
 	io.println(JSON.stringify(actor.read()));
+	sleep.sleep("1s");
 }
